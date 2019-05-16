705146d0a62bf2b426e89728f0f5923683c229ab
OlgaJP <olga.pavlova@jetbrains.com>
2019-05-16 15:13:09 +0200

    changed, removed some not needed files

--- src/Java_echo_app_args.java
+++ src/Java_echo_app_args.java
@@ -2,14 +2,14 @@ import java.util.Date;
 
 public class Java_echo_app_args {
     public static void main (String[] args) {
-        System.out.println("Args of this program," + new Date().toString() + ":" );
+        System.out.println("Date: " + new Date().toString() + ". \nArgs of this program:" );
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         for (String s: args) {
-            System.out.println(s + " changed");
+            System.out.println( s );
         }
     }
 
